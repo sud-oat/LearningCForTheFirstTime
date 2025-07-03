b8819f29ffe7ac2ba1f8468d98f042b66382b889
Sud-OAT <60008527+sud-oat@users.noreply.github.com>
2025-07-03 13:00:12 +0530

    Update StackOverFlowUnderFlow.c

--- StackOverFlowUnderFlow.c
+++ StackOverFlowUnderFlow.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int push(stack, pointer, x){
-  if (pointer >= 10){
-  	printf("Stack Overflow");
-    return false;
-  }else {
-  	printf("Pushed");
-    stack[pointer] = x;
-    pointer +=1;
-    return true;
-  }
+bool push(int stack[], int *pointer, int x) {
+    if (*pointer >= 10) {
+        printf("Stack Overflow\n");
+        return false;
+    } else {
+        stack[*pointer] = x;
+        printf("Pushed %d\n", x);
+        (*pointer)++;
+        return true;
+    }
 }
-int pop (stack, pointer){
-  if (pointer <=0){
-  	printf("Stack Underflow");
-	return false;
-  }else {
-  	printf("Popped");
-    stack[pointer] = 0;
-    pointer -= 1;
-    return true;
+
+bool pop(int stack[], int *pointer) {
+    if (*pointer <= 0) {
+        printf("Stack Underflow\n");
+        return false;
+    } else {
+        (*pointer)--;
+        printf("Popped %d\n", stack[*pointer]);
+        stack[*pointer] = 0;
+        return true;
+    }
 }
 
 int main() {
-  int stack[10];
-  int pointer;
-  int x;
-  printf("Do you want to push or pop? 0 for pop 1 for push");
-  int choice;
-  scanf("$d", &choice);
-  if (choice == 0){
-  	printf("Enter the value to push");
-    scanf("%d", &x);
-  	push(stack, pointer, x);
-  } else {
-  	pop(stack, pointer);
-  return 0;
+    int stack[10] = {0};
+    int pointer = 0;
+    int x;
+    int choice;
+
+    printf("Do you want to push or pop? 0 for pop, 1 for push: ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        printf("Enter the value to push: ");
+        scanf("%d", &x);
+        push(stack, &pointer, x);
+    } else {
+        pop(stack, &pointer);
+    }
+
+    return 0;
 }
